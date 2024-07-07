2f2d101f7f7d188a64b367c9e4b56e379a156bef
Unique Neupane <133880428+Unique982@users.noreply.github.com>
2024-07-07 09:06:20 +0545

    Delete lab 1 directory
    delete

--- lab 1/add.cs
+++
@@ -1,8 +0,0 @@
-// Write C# program to add, subtract, multiply and division of two number using input from command Line.
-using System;
-public class Add{
-public static void Main(String []args){
-Console.WritLine("Enter any Number"){
-int a= int.parse(Console.ReadLine());
-int b = int.parse(Console.ReadLine());
-Console.WriteLine("Enter operator");
